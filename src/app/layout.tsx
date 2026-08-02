cc39864b2184f07564bb6a86abad3f5648eac6a1
Kelson Cosme <kelson.almeida123@gmail.com>
2026-08-02 19:24:54 -0400

    feat: clarity instalado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 import Navbar from "@/components/Navbar";
 import SplashScreen from "@/components/SplashScreen";
 import SmoothScrolling from "@/components/SmoothScrolling";
@@ -49,6 +50,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${inter.variable} ${playfair.variable} ${georgia.variable} ${madeTommy.variable}`}>
+      <head>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xwb9rb4rfq");
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning>
         <SmoothScrolling>
           <SplashScreen />
